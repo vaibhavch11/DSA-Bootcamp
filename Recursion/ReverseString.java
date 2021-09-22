fdd106a720f5cc364e8b246a44aded5518b8ca8a
vaibhavch11 <66193463+vaibhavch11@users.noreply.github.com>
2021-09-22 22:28:01 +0530

    Update ReverseString.java

---
+++ Recursion/ReverseString.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] arr) {
+        int left = 0;
+        int right = arr.length-1;
+        
+        
+        while(left<=right){
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            
+             left++;
+             right--;
+        }
+       
+        
+    }
+}
+[https://leetcode.com/problems/reverse-string/]
